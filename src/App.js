1b354238cef25a7122b9b88c4857c5849a727ca2
kilcmn <kilcmn3@hotmail.com>
2020-09-02 13:23:07 -0400

    succefully hide api key

--- src/App.js
+++ src/App.js
@@ -9,19 +9,24 @@ function App() {
     script.async = true;
     script.src =
       'https://dapi.kakao.com/v2/maps/sdk.js?appkey=' +
-      process.env.KAKAO_API +
+      process.env.REACT_APP_KAKAO_API +
       '&autoload=false';
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+    document.head.appendChild(script);
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    script.onload = () => {
+      kakao.maps.load(() => {
+        const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        const options = {
+          //지도를 생성할 때 필요한 기본 옵션
+          center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+          level: 3, //지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      });
+    };
   }, []);
 
-  console.log(process.env.REACT_APP_KAKAO_API);
   return (
     <div className='App'>
       <div id='map' style={{ width: '500px', height: '500px' }}></div>
