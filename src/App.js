ea4c0b4baf665c92c8525a8e55eae0ea2c057513
kilcmn <kilcmn3@hotmail.com>
2020-09-09 12:46:37 -0400

    before install cors

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
 import React, { useEffect } from 'react';
 import MapContainer from './MapContainer';
 import './App.css';
+var xhr = new XMLHttpRequest();
 
 const App = () => {
-  // var url =
-  //   'http://apis.data.go.kr/B552061/schoolzoneChild/getRestSchoolzoneChild';
-  // var queryParams =
-  //   '?' +
-  //   encodeURIComponent('ServiceKey') +
-  //   '=' +
-  //   process.env.REACT_APP_DATA_API; /* Service Key*/
-  // queryParams +=
-  //   '&' +
-  //   encodeURIComponent('ServiceKey') +
-  //   '=' +
-  //   encodeURIComponent('인증키(URL Encode)'); /* */
-  // queryParams +=
-  //   '&' +
-  //   encodeURIComponent('searchYearCd') +
-  //   '=' +
-  //   encodeURIComponent('2019'); /* */
-  // queryParams +=
-  //   '&' + encodeURIComponent('siDo') + '=' + encodeURIComponent('11'); /* */
-  // queryParams +=
-  //   '&' + encodeURIComponent('guGun') + '=' + encodeURIComponent('320'); /* */
-  // queryParams +=
-  //   '&' + encodeURIComponent('type') + '=' + encodeURIComponent('xml'); /* */
-  // queryParams +=
-  //   '&' +
-  //   encodeURIComponent('numOfRows') +
-  //   '=' +
-  //   encodeURIComponent('10'); /* */
-  // queryParams +=
-  //   '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+  var url =
+    'http://apis.data.go.kr/B552061/schoolzoneChild/getRestSchoolzoneChild'; /*URL*/
+  var queryParams =
+    '?' +
+    encodeURIComponent('ServiceKey') +
+    '=' +
+    process.env.REACT_APP_DATA_API; /*Service Key*/
+  queryParams +=
+    '&' +
+    encodeURIComponent('ServiceKey') +
+    '=' +
+    encodeURIComponent('인증키(URL Encode)'); /**/
+  queryParams +=
+    '&' +
+    encodeURIComponent('searchYearCd') +
+    '=' +
+    encodeURIComponent('2015'); /**/
+  queryParams +=
+    '&' + encodeURIComponent('siDo') + '=' + encodeURIComponent('11'); /**/
+  queryParams +=
+    '&' + encodeURIComponent('guGun') + '=' + encodeURIComponent('320'); /**/
+  queryParams +=
+    '&' + encodeURIComponent('type') + '=' + encodeURIComponent('xml'); /**/
+  queryParams +=
+    '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+  queryParams +=
+    '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
 
   //calling on every render
+  useEffect(() => {
+    xhr.open('GET', url + queryParams);
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        console.log(
+          'Status: ' +
+            this.status +
+            'nHeaders: ' +
+            JSON.stringify(this.getAllResponseHeaders()) +
+            'nBody: ' +
+            this.responseText
+        );
+      }
+    };
 
+    xhr.send('');
+  });
   return (
     <div className='App'>
       <MapContainer />
