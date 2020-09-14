3a7b55ee8d98a7672c70f55caa1c94f2c2fab1b4
kilcmn <kilcmn3@hotmail.com>
2020-09-14 12:03:28 -0400

    changing <script> to HTML file

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -2,15 +2,14 @@
 import React, { useEffect } from 'react';
 
 const MapContainer = (props) => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src =
+    'https://dapi.kakao.com/v2/maps/sdk.js?appkey=' +
+    process.env.REACT_APP_KAKAO_API +
+    '&autoload=false';
+  document.head.appendChild(script);
   useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src =
-      'https://dapi.kakao.com/v2/maps/sdk.js?appkey=' +
-      process.env.REACT_APP_KAKAO_API +
-      '&autoload=false';
-    document.head.appendChild(script);
-
     script.onload = () => {
       kakao.maps.load(() => {
         const container = document.getElementById('map');
@@ -21,42 +20,36 @@ const MapContainer = (props) => {
         };
 
         const map = new window.kakao.maps.Map(container, options); //Instance of new map
-
-        //The coordinates of the marker
-        // const marketPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-
-        //List of marker's position and title of that place.
-        const positions = [
-          {
-            title: '카카오',
-            latlng: new kakao.maps.LatLng(33.450705, 126.570677),
-          },
-          {
-            title: '생태연못',
-            latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-          },
-          {
-            title: '텃밭',
-            latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-          },
-          {
-            title: '근린공원',
-            latlng: new kakao.maps.LatLng(33.451393, 126.570738),
-          },
-        ];
-
-        //creat a marker
-        for (let i = 0; i < positions.length; i++) {
-          const marker = new kakao.maps.Marker({
-            map: map, //This will shows the marker on the map
-            position: positions[i].latlng,
-            title: positions[i].title,
-          });
-        }
       });
     };
   }, []);
 
+  //The coordinates of the marker
+  // const marketPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+  //List of marker's position and title of that place.
+  const positions = (data) => {
+    const marketContainer = [];
+    data = props.coordinates;
+    console.log('inside the position');
+    for (let i = 0; i < data.length; i++) {
+      let position = new Object();
+      position.latlng = new kakao.maps.Latlng(data[i][0], data[i][1]);
+      marketContainer.push(position);
+    }
+    return marketContainer;
+  };
+
+  //creat a marker
+  for (let i = 0; i < positions().length; i++) {
+    const marker = new kakao.maps.Marker({
+      map: map, //This will shows the marker on the map
+      position: positions()[i].latlng,
+      title: positions()[i].title,
+    });
+  }
+
+  console.log(props.coordinates);
   return <div id='map'></div>;
 };
 
