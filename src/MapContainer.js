a97db5a9d651ac776ffa369549afe66a3166afcd
kilcmn <kilcmn3@hotmail.com>
2020-09-15 13:40:18 -0400

    seems coordinates look like a circle...need to figure out if the given data is right or not

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -47,26 +47,6 @@ const MapContainer = (props) => {
     }
   };
 
-  //creat a marker
-  // var positions = [
-  //   {
-  //     title: '카카오',
-  //     latlng: new kakao.maps.LatLng(33.450705, 126.570677),
-  //   },
-  //   {
-  //     title: '생태연못',
-  //     latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-  //   },
-  //   {
-  //     title: '텃밭',
-  //     latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-  //   },
-  //   {
-  //     title: '근린공원',
-  //     latlng: new kakao.maps.LatLng(33.451393, 126.570738),
-  //   },
-  // ];
-
   return <div id='map'></div>;
 };
 
